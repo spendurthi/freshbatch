cb18d1bfda63f70bb3edfffbaec2d460189c1f17
pendurthis <pendurthis@SuryaPC>
2016-12-26 19:15:24 -0500

    1

---
+++ src/main/java/com/sone/freshbatch/freshbatch/App.java
@@ -0,0 +1,13 @@
+package com.sone.freshbatch.freshbatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
